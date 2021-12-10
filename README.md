eed6bff58873dc17ba7dbe3938d1f195758657d2
Akshen Kadakia <akshenk8@gmail.com>
2021-12-10 15:27:17 -0800

    form validation fixes

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## Setup
 
-1. Install Python 3.9.
+1. Install Python 3.9
 2. Create a virtual environment using `virtualenv` or `conda`. Then install the requirements mentioned in
    the `requirements.txt`.
 3. This project uses sqlite DB and doesn't require any additional databases.
@@ -9,7 +9,9 @@
 
 ## Running the server
 
-1. Execute the following command `python manage.py runserver`
+1. Execute the following command `python manage.py runserver`. (not ideal for production. for deployment
+   refer [this](https://docs.djangoproject.com/en/4.0/howto/deployment/) and NGINX or Apache for deploying static files)
+   .
 2. Go to `http://127.0.0.1:8000/admin/` and login with the username and password. As I haven't created the login &
    logout pages for user accounts.
 3. Then go to `http://127.0.0.1:8000/` for testing the Team management application.
--- TeamApplication/models.py
+++ TeamApplication/models.py
@@ -1,7 +1,11 @@
 from django.utils.translation import gettext_lazy as _
 from django.contrib.auth import get_user_model
-from django.core import validators
+from django.core.exceptions import ValidationError
 from django.db import models
+
+import re
+
+phone_regex = re.compile(r'[0-9]{3}-[0-9]{3}-[0-9]{4}$')
 
 User = get_user_model()
 
@@ -11,11 +15,23 @@ class TeamMemberRoles(models.TextChoices):
     ADMIN = "admin", _('Admin - Can delete members')
 
 
+def validate_phone_number(value):
+    if phone_regex.fullmatch(value) is None:
+        raise ValidationError(
+            _('%(value)s is not a phone number'),
+            params={'value': value},
+        )
+
+
 class TeamMember(models.Model):
     first_name = models.CharField(max_length=64)
     last_name = models.CharField(max_length=64)
-    email_id = models.CharField(max_length=64, validators=[validators.EmailValidator])
-    mobile_number = models.CharField(max_length=15, help_text="Please use the following format: <em>XXX-XXX-XXXX</em>.")
+    email_id = models.EmailField(max_length=64)
+    mobile_number = models.CharField(
+        max_length=15,
+        help_text="Please use the following format: <em>XXX-XXX-XXXX</em>.",
+        validators=[validate_phone_number]
+    )
     role = models.CharField(max_length=10, choices=TeamMemberRoles.choices, default=TeamMemberRoles.REGULAR)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
